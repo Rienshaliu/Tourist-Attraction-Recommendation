ef2390276b33ea2121021b76990ee31fc9fbd622
Rienshaliu <rienshaliu@gmail.com>
2018-02-09 11:38:50 +0800

    output files

--- instagram crawler/instagram_crawler.py
+++ instagram crawler/instagram_crawler.py
@@ -11,11 +11,24 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from collections import defaultdict
+import collections
+from operator import itemgetter
+from collections import OrderedDict
 
-filepath = "./output"
-f = open(filepath, 'w', encoding="utf-8")
-filepath1 = "./login"
-f1 = open(filepath1, 'r', encoding="utf-8")
+locations = defaultdict(int)
+
+post_id = 0
+
+userid = 0
+userpath = "./user%d"%userid
+user_file = open(userpath, 'w', encoding="utf-8")
+
+imgpath = "./img%d"%userid
+img_file = open(imgpath, 'w', encoding="utf-8")
+
+loginpath = "./login"
+login_file = open(loginpath, 'r', encoding="utf-8")
 
 class InstagramUser:
     def __init__(self, user_id, username=None, bio=None, followers_count=None, following_count=None, is_private=False, user_picture=None):
@@ -52,13 +65,14 @@ def get_profile_information(data):
 
 if __name__ == '__main__':
     
-    login_data = f1.read()
+    login_data = login_file.read()
     print(login_data)
     login = []
     login = re.split(' ',login_data)
     username = login[0]
     password = login[1]
     tag = "jojoxdaily"
+    img_file.write(tag)
     
     quote_page = "https://www.instagram.com/%s/"%tag
     base_url = "https://www.instagram.com"
@@ -98,11 +112,12 @@ if __name__ == '__main__':
 
     ele_posts = []
     ele_posts = browser.find('._mck9w')
-    while len(ele_posts) < 24:
+    while len(ele_posts) < 12:
         browser.scroll_down()
         ele_posts = browser.find('._mck9w')
         #print(len(ele_posts))
-    pageSource = browser.driver.page_source
+    
+    '''pageSource = browser.driver.page_source
     #print(len(ele_posts))
     #f.write("%s"%pageSource)
     
@@ -111,7 +126,7 @@ if __name__ == '__main__':
     shared_data = extract_shared_data(response)
     user_profile = get_profile_information(shared_data)
     pprint(vars(user_profile))
-    #f.write("%s"%shared_data)
+    #f.write("%s"%shared_data)'''
     
     #sleep(800)
     rows = browser.find('._6d3hm._mnav9')
@@ -123,36 +138,50 @@ if __name__ == '__main__':
     
     print(len(links))
     
-    for j in range(0, len(links)):
-        variable = re.sub(base_url, "", links[j])
-        browser.driver.find_element_by_xpath("//a[@href='%s']"%variable).click()
-        sleep(2)
-        location = browser.find('._q8ysx._6y8ij')
-        imgs = browser.find('._2di5p')
-        if location:
-            location_url = location[0].get_attribute('href')
-            f.write("\nlocation_url: %s"%location_url)
-            
-            imgs = browser.find('._2di5p')
-            if imgs:
-                img_url = imgs[len(imgs)-1].get_attribute('src')
-                f.write("\nimg_url: %s"%img_url)
-            
-            taged_people = browser.find('._n1lhu._4dsc8')
-            if taged_people:
-                taged_users_url = []
-                for p in taged_people:
-                    taged_users_url.append(p.get_attribute('href'))
-                f.write("\ntaged_users_url: %s"%taged_users_url)
-            
-            date_time = browser.find_one('._p29ma._6g6t5').get_attribute('datetime')
-            f.write("\ndate_time: %s"%date_time)
-            
-        #sleep(800)
-        close_btn = browser.find_one('._dcj9f')
-        close_btn.click()
-        sleep(2)
+    #for j in range(0, len(links)):
+    for j in range(0, 3):
+        
         contents = browser.find('._4rbun')
         content = contents[j].find_element_by_tag_name('img').get_attribute('alt')
-        f.write("\ncontent: %s"%content)
-        f.write("\n\n")
+        prize = 0
+        if content.find('抽獎') >= 0:
+            prize = 1
+            print("======prize======")
+        if prize == 0:
+            variable = re.sub(base_url, "", links[j])
+            browser.driver.find_element_by_xpath("//a[@href='%s']"%variable).click()
+            sleep(2)
+            location = browser.find('._q8ysx._6y8ij')
+            imgs = browser.find('._2di5p')
+            if location and imgs:
+                
+                postpath = "./post%d"%post_id
+                post_file = open(postpath, 'w', encoding="utf-8")
+                post_file.write("萌萌\n")
+                post_file.write("%s\n"%links[j])
+                
+                location_url = location[0].get_attribute('href')
+                locations[location_url]+=1
+                img_file.write("\n%s"%location_url)
+                post_file.write("%s\n"%location_url)
+                
+                img_url = imgs[len(imgs)-1].get_attribute('src')
+                img_file.write("\n%s"%img_url)
+                
+                taged_people = browser.find('._n1lhu._4dsc8')
+                if taged_people:
+                    taged_users_url = []
+                    for p in taged_people:
+                        taged_users_url.append(p.get_attribute('href'))
+                    #f.write("\ntaged_users_url: %s"%taged_users_url)
+                '''
+                date_time = browser.find_one('._p29ma._6g6t5').get_attribute('datetime')
+                f.write("\ndate_time: %s"%date_time)'''
+                post_file.write("%s"%content)
+                post_file.close()
+                post_id+=1
+                
+            #sleep(800)
+            close_btn = browser.find_one('._dcj9f')
+            close_btn.click()
+            sleep(2)
